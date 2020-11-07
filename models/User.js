e70abced56c394a6aee5abf7d153e220ba0cf967
BOKYUN <bokyun@MacBook-Pro.local>
2020-11-07 23:10:15 +0900

    회원가입/로그인

--- models/User.js
+++ models/User.js
@@ -1,4 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose');  // MonggoDB
+
+const bcrypt = require('bcrypt');      // 비밀번호 암호화
+const saltRounds = 10;                 // salt 길이           
+
+const jwt = require('jsonwebtoken');
 
 // 스키마 생성
 const userSchema = mongoose.Schema({
@@ -31,6 +36,51 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 });
+
+// Schema.pre =>  save 이전 단계에 실행, next에 도달하면 save 진행
+userSchema.pre('save', function(next){
+    
+    var user = this;
+
+    // 비밀번호 변경 시에만 암호화
+    if(user.isModified('password')){
+
+        // 비밀번호를 암호화 시킨다.
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err); // 에러 발생 시 에러 그대로 return
+
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err);
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+});
+
+// 비밀번호 비교
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    // plainPassword : 입력 비밀번호
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err);
+        cb(null, isMatch);
+    });
+}
+
+// 로그인 토큰 발행
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+
+    // jwt 토큰 생성
+    var token = jwt.sign(user._id.toHexString(), 'secretToken');
+    user.token = token;
+    user.save(function(err, user){
+        if(err) return cb(err);
+        cb(null, user)
+    })
+};
 
 // 모델 생성(모델로 스키마 감싸기)
 const User = mongoose.model('User', userSchema);
