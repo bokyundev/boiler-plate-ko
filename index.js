faac47075d0f9fb57d1367f90848ac5e6bef5e36
BOKYUN <bokyun@MacBook-Pro.local>
2020-11-07 15:50:48 +0900

    회원가입 기능

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const app = express();
 // 포트 지정
 const port = 5000;
 
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+// bodyParser 설정 : client <-> server간 통신
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+// application/json
+app.use(bodyParser.json());
+
 // MongoDB 연결
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://bokyundev:bokyun1234@boilerplate.ote0a.mongodb.net/<dbname>?retryWrites=true&w=majority', {
@@ -15,5 +24,19 @@ mongoose.connect('mongodb+srv://bokyundev:bokyun1234@boilerplate.ote0a.mongodb.n
   .catch(err => console.log(err));                 // catch(err) : 에러
 
 app.get('/', (req, res) => res.send('Hello World~~~!'));
+
+app.post('/register', (req, res) => {
+
+    // 회원 가입 할 때 필요한 정보를 client에서 가져오면
+    // 그것들을 데이터베이스에 넣어준다.
+    const user = new User(req.body);
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err});
+        return res.status(200).json({
+            success: true
+        })
+    })
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
